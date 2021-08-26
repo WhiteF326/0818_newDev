ebb106c1d1fa75d32b65adb30e0f56beeefb62ff
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-26 11:44:58 +0900

    Clean: Refactoring

--- js/main.js
+++ js/main.js
@@ -1,27 +1,41 @@
+// tile size
+const tilesize = 32;
+
 //canvas設定
-let canvas = document.getElementById('canvas');
-canvas.width = 288;		//canvasの横幅
-canvas.height = 288;	//canvasの縦幅
+const canvas = document.getElementById('canvas');
+canvas.width = tilesize * map[0].length;		//canvasの横幅
+canvas.height = tilesize * map.length;      	//canvasの縦幅
 
 //コンテキスト取得
-let ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 //マップタイル用Imageオブジェクトの生成
 let maptile_wall = new Image();
-maptile_wall.src = 'img/map01.png';
+// maptile_wall.src = 'img/map01.png';
+maptile_wall.setAttribute("src", "img/map01.png");
 
 let maptile_floor = new Image();
-maptile_floor.src = 'img/map02.png';
-
-//マップ
-document.write("<script src='map.js'></script>");
+// maptile_floor.src = 'img/map02.png';
+maptile_floor.setAttribute("src", "img/map02.png");
 
 //マップタイルの表示
-addEventListener('load', function () {
+addEventListener('load', () => {
     for (let y = 0; y < map.length; y++) {
         for (let x = 0; x < map[y].length; x++) {
-            if (map[y][x] === 0) ctx.drawImage(maptile_wall, 0, 0, 32, 32, 32 * x, 32 * y, 32, 32);
-            if (map[y][x] === 1) ctx.drawImage(maptile_floor, 0, 0, 32, 32, 32 * x, 32 * y, 32, 32);
+            if (map[y][x] === 0) ctx.drawImage(
+                maptile_wall,
+                0, 0,
+                maptile_wall.naturalWidth, maptile_wall.naturalHeight,
+                tilesize * x, tilesize * y,
+                tilesize, tilesize
+            );
+            if (map[y][x] === 1) ctx.drawImage(
+                maptile_floor,
+                0, 0,
+                maptile_floor.naturalWidth, maptile_floor.naturalHeight,
+                tilesize * x, tilesize * y,
+                tilesize, tilesize
+            );
         }
     }
 }, false);
