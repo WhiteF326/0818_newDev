49dc8eb75ba9295f6b274d217793922fd4915dd2
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-26 13:34:30 +0900

    Clean: rename Body

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-import { Server } from "https://js.sabae.cc/Server.js";
+import {Server} from 'https://js.sabae.cc/Server.js';
 
-class body extends Server {
-    async api(path, prm) {
-        let ret = null;
-        switch (path.split("/")[2]) {
-            case "":
-                break;
+class Body extends Server {
+  async api(path, prm) {
+    let ret = null;
+    switch (path.split('/')[2]) {
+      case '':
+        break;
 
-            case "stage":
-                ret = await Deno.readTextFile("./stage/" + prm.name + ".json");
-                break;
-        }
-        return ret;
+      case 'stage':
+        ret = await Deno.readTextFile('./stage/' + prm.name + '.json');
+        break;
     }
+    return ret;
+  }
 }
 
-new body(4478);
+new Body(4478);
