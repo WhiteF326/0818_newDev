d350ba61a1340afddb14b369a779378a2224ff05
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-26 16:06:23 +0900

    Clean: fix by lint

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import { Server } from 'https://js.sabae.cc/Server.js';
+import {Server} from 'https://js.sabae.cc/Server.js';
 
 class Body extends Server {
   async api(path, prm) {
     let ret = null;
     switch (path.split('/')[2]) {
-      case '':
-        break;
+    case '':
+      break;
 
-      case 'stage':
-        ret = await Deno.readTextFile('./stage/' + prm.name + '.json');
-        break;
+    case 'stage':
+      ret = await Deno.readTextFile('./stage/' + prm.name + '.json');
+      break;
     }
     return ret;
   }
