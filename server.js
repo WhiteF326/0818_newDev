f2866f8d0acbc3705b821b549edbb7bdc17285f5
WhiteF326 <Lume1a326.leno@gmail.com>
2021-08-26 11:55:24 +0900

    Clean: Fix indent

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import { Server } from "https://js.sabae.cc/Server.js";
 
-class body extends Server{
-  async api(path, prm){
-    let ret = null;
-    switch(path.split("/")[2]){
-      case "":
-        break;
-      
-      case "stage":
-        ret = await Deno.readTextFile("./stage/" + prm.name + ".json");
-        break;
+class body extends Server {
+    async api(path, prm) {
+        let ret = null;
+        switch (path.split("/")[2]) {
+            case "":
+                break;
+
+            case "stage":
+                ret = await Deno.readTextFile("./stage/" + prm.name + ".json");
+                break;
+        }
+        return ret;
     }
-    return ret;
-  }
 }
 
 new body(4478);
