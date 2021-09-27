d0a14f4715b8e6f153ed0830d992be636d19092c
YabushitaRyoto <yabushita.ryoto.t20ohf@gmail.com>
2021-09-27 15:15:05 +0900

    commit

--- static/js/Doors.js
+++ static/js/Doors.js
@@ -1,15 +1,16 @@
-class Doors{
-  constructor(ary){
+class Doors {
+  constructor(ary) {
     this.last = ary;
   }
 
-  pushed(ptr){
+  pushed(ptr) {
+    switchSound.currentTime = 0;
     switchSound.play();
     this.last[ptr] = max(0, this.last[ptr] - 1);
     // console.log(this.last);
-    if(!this.last[ptr]){
+    if (!this.last[ptr]) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
