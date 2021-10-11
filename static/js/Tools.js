0fa5bfe8f2eaa951ec55b70d3cfb45a7a63ec404
WhiteF326 <Lume1a326.leno@gmail.com>
2021-10-11 09:41:06 +0900

    ステージセレクトの色味を変更

--- static/js/Tools.js
+++ static/js/Tools.js
@@ -1,8 +1,8 @@
 const hsv2rgb = (hsv) => {
   const h = hsv[0] / 60;
   const s = hsv[1];
-  const v = hsv[2];
-  if(s === 0) return [v * 255, v * 255, v * 255];
+  const v = hsv[2] >= 1 ? 1 : hsv[2];
+  if (s === 0) return [v * 255, v * 255, v * 255];
 
   let rgb = [];
   const i = parseInt(h);
@@ -13,16 +13,16 @@ const hsv2rgb = (hsv) => {
     v * (1 - s * (1 - f))
   ];
 
-  switch(i){
+  switch (i) {
     case 0:
     case 6:
       rgb = [v, vl[2], vl[0]];
       break;
-    
+
     case 1:
       rgb = [vl[1], v, vl[0]];
       break;
-    
+
     case 2:
       rgb = [vl[0], v, vl[2]];
       break;
@@ -30,11 +30,11 @@ const hsv2rgb = (hsv) => {
     case 3:
       rgb = [vl[0], vl[1], v];
       break;
-    
+
     case 4:
       rgb = [vl[2], vl[0], v];
       break;
-    
+
     case 5:
       rgb = [v, vl[0], vl[1]];
       break;
@@ -48,17 +48,22 @@ const hsv2rgb = (hsv) => {
   return rgbTxt;
 }
 
-const retColor = (p) => {
-  const hues = Math.floor(p * 360 / 7);
+const hues = (p, e) => {
+  console.log(Math.floor(120 / (e - 1)) * p);
+  return 120 - Math.floor(120 / (e - 1)) * p;
+}
+
+const retColor = (p, e, d) => {
+  const sdiff = d >= 6 ? 0.15 : 0;
   return {
-    0: hsv2rgb([hues, 0.79, 0.77]),
-    20: hsv2rgb([hues, 0.66, 0.93]),
-    40: hsv2rgb([hues, 0.07, 0.96]),
-    60: hsv2rgb([hues, 0.66, 0.93]),
-    80: hsv2rgb([hues, 0.79, 0.77]),
+    0: hsv2rgb([hues(p, e), 0.79, 0.77 + sdiff]),
+    20: hsv2rgb([hues(p, e), 0.66, 0.93 + sdiff]),
+    40: hsv2rgb([hues(p, e), 0.07, 0.96 + sdiff]),
+    60: hsv2rgb([hues(p, e), 0.66, 0.93 + sdiff]),
+    80: hsv2rgb([hues(p, e), 0.79, 0.77 + sdiff]),
   }
 }
 
 const retTextColor = (p) => {
-  return ["white", "black", "black", "black", "black", "white", "black"][p]
+  return "white";
 }
