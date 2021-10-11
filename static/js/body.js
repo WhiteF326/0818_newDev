ccc9e5962d1a6ff14a760b1936bee3e62be59168
WhiteF326 <Lume1a326.leno@gmail.com>
2021-10-11 16:29:20 +0900

    応急対処

--- static/js/body.js
+++ static/js/body.js
@@ -61,7 +61,7 @@ class Body {
 
     // tile size
     this.tilesize = min(
-      64, floor(this.sh / 2.2 / Math.max(this.map.length, this.map[0].length)));
+      64, floor(this.sh / 2.4 / Math.max(this.map.length, this.map[0].length)));
 
     // キャラ情報の取得
     this.charaAuto = new CharaAuto(this.start[2], this.tilesize, settings);
