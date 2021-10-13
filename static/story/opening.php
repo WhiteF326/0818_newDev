cb1827a7d72f1daffd3afd661a9ccfdb0ed8dfdd
YabushitaRyoto <yabushita.ryoto.t20ohf@gmail.com>
2021-10-13 16:04:09 +0900

    commit

--- static/story/opening.php
+++ static/story/opening.php
@@ -1,33 +1,32 @@
 <?php
-require_once("./../../settings/env.php");
-//セリフ入れる配列
-$serif = [];
-//セリフ番号
-$serifId = 1;
-//サーバーからセリフファイルを呼び出してstory.jsに送る
-$username = "LAA1355306";
-$database = "LAA1355306-proggame";
-$password = $env["dbPassword"];
-$dsn = "mysql:dbname=" . $database . ";host=localhost";
+$serif = []; //セリフ入れる配列
+$serifId = 1; //セリフ番号
+$faceNum = []; //表情数値
 
-$pdo = new PDO($dsn, $username, $password);
+//データベースからセリフファイルを呼び出してstory.jsに送る
+$dsn = "mysql:dbname=programgame;localhost=3306";
+$user = "user";
+$pass = "user002";
 try {
-  $pdo = new PDO($dsn, $username, $password);
-  //sql文作成
-  $sql = "SELECT serif_file FROM message
+    $pdo = new PDO($dsn, $user, $pass);
+    //sql文作成
+    $sql = "SELECT serif_file,face_file,back_img FROM message
     WHERE $serifId = serif_id";
-  //sql実行
-  $stmt = $pdo->query($sql);
+    //sql実行
+    $stmt = $pdo->query($sql);
 
-  foreach ($stmt as $row) {
-    $serif[] = explode(",", $row["serif_file"]);
-  }
+    foreach ($stmt as $row) {
+        $faceNum[] = explode(",", $row["face_file"]);
+        $serif[] = explode(",", $row["serif_file"]);
+        $imgPath = $row["back_img"];
+    }
 } catch (PDOException $e) {
-  echo $e->getMessage();
+    echo $e->getMessage();
 }
 
 
-$js_array = json_encode($serif);
+$serif_array = json_encode($serif);
+$faceNum_array = json_encode($faceNum);
 
 ?>
 
@@ -35,148 +34,196 @@ $js_array = json_encode($serif);
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>オープニング</title>
-  <link rel="stylesheet" href="style.css">
-  <script type="text/javascript" src="./../js/LoginCheck.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ストーリー</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
-<body>
-  <div id="area">
-    <canvas id="can" width="850" height="530"></canvas>
-  </div>
+<body onload="message()">
+    <div id="area">
+        <canvas id="can" width="850" height="530"></canvas>
+    </div>
 
-  <br>
+    <br>
 
-  <div id="serifarea"></div>
-  <br>
-  <button id="startBtn" onclick="message()">始める</button>
-  <button onclick="skip()">オープニングをスキップする</button>
-  <div id="nextarea"></div>
+    <div id="serifarea"></div>
+    <div class="Btnarea" id="nextarea">
+        <button id="startBtn" onclick="message()" class="Btn_1">進む</button>
+        <button onclick="skip()" class="Btn_2">ストーリーをスキップする</button>
+    </div>
 
-  <script>
-    //キャンバスの作成
-    const canvas = document.getElementById("can");
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillRect(0, 0, 850, 530);
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "rgb(255,255,255)";
-    ctx.fillText("始めるボタンを押してストーリーを読んで下さい。", 0, 100);
+    <script>
+        //キャンバスの作成
+        const canvas = document.getElementById("can");
+        const ctx = canvas.getContext("2d");
 
-    //画像の取得
-    let backImg = new Image();
-    let youseiImg = new Image();
-    let robotImg = new Image();
-    //パス設定
-    backImg.src = "image/morigarakuta.png";
-    youseiImg.src = "image/yousei.png";
-    robotImg.src = "image/robo.png";
+        //画像の取得
+        let backImg = new Image();
+        let youseiImg = new Image();
+        let robotImg = new Image();
+        let path = '<?php echo $imgPath; ?>';
+        //console.log(typeof(path));
 
+        //画像を読み込む前に描画しないようにloadイベントを行う
+        backImg.onload = function() {
+            //画像の描画
+            ctx.drawImage(backImg, 0, 0, 850, 530);
+        }
+        backImg.src = path;
 
-    //画像を読み込む前に描画しないようにloadイベントを行う
-    /*backImg.onload = function() {
-        //画像の描画
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-    }
+        youseiImg.onload = function() {
+            ctx.drawImage(youseiImg, 400, 200, 450, 600);
+        }
+        youseiImg.src = "image/yousei.png";
 
-    robotImg.onload = function() {
-        ctx.drawImage(robotImg, 0, 200, 450, 600);
-    }
+        robotImg.onload = function() {
+            ctx.drawImage(robotImg, 0, 200, 450, 600);
+        }
+        robotImg.src = "image/robo.png";
 
-    youseiImg.onload = function() {
-        ctx.drawImage(youseiImg, 400, 200, 450, 600);
-    }*/
+        /*ctx.fillStyle = "rgb(0,0,0)";
+        ctx.fillRect(0,0,850,530);
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "rgb(255,255,255)";
+        ctx.fillText("ストーリーを読むには、進むボタンを押してください。", 10, 100);
+        */
 
+        //PHPから配列取ってくる
+        let serif_array = <?php echo $serif_array; ?>;
+        let face_array = <?php echo $faceNum_array; ?>;
 
-    //PHPから配列取ってくる
-    let array = <?php echo $js_array; ?>;
-    let speed = 100; //ミリ秒単位
-    let string, char;
-    let strlen = array[0].length - 1;
-    let req;
-    let i = 0;
+        let speed = 100; //ミリ秒単位
+        let string;
+        let faceNum;
+        let strlen = serif_array[0].length - 1;
+        let i = 0; //セリフ位置
 
-    console.log(array[0]);
-
-    function message() {
-      //始めるボタンの無効化
-      const startBtn = document.getElementById("startBtn");
-      startBtn.disabled = true;
-
-      //セリフ配列から一つ目のセリフを取り出す
-      string = array[0].slice(0, 1).toString();
-      array[0] = array[0].slice(1);
-
-      message_char();
-
-    }
-
-    function message_char() {
-      //console.log(i);
-      //char = string.slice(0, i);
-      //console.log(char);
-      const serif = document.getElementById("serifarea");
-
-      if (string.indexOf("妖精", 0) != -1) {
-        //妖精が喋っているとき
-        //全体のcanvasを削除
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //再描画
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-        ctx.drawImage(youseiImg, 400, 200, 450, 600);
-
-      } else if (string.indexOf("アンドロイド", 0) != -1) {
-        //アンドロイドが喋っているとき
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-        ctx.drawImage(robotImg, 0, 200, 450, 600);
-      } else {
-        //それ以外
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-      }
-
-      //セリフ表示
-      serif.innerHTML = string;
-
-      //セリフが最後まで終わっているか
-      if (i <= string.length) {
-        i++;
-        Timer();
-      } else { //終わったとき
-        if (array[0].length == 0) {
-          const next = document.getElementById("nextarea");
-          const btn = document.createElement("button");
-          btn.innerHTML = "ゲームスタート！";
-          btn.onclick = function() {
-            //ボタンが押された時
-            localStorage.setItem("gameEnabled", "story");
-            location = "./../toStage.html";
-          };
-          next.appendChild(btn);
-          //console.log("終わり");
-          return;
+        //表情判定関数
+        function faceJudge(num, name) {
+            switch (num) {
+                case "1": //表情:普通
+                    if (name == "妖精") {
+                        return "image/yousei.png";
+                    } else {
+                        return "image/robo.png";
+                    }
+                    break;
+                case "2": //笑顔
+                    if (name == "妖精") {
+                        return "image/nikkoriyousei.png";
+                    } else {
+                        return "image/nikkorirobo.png";
+                    }
+                    break;
+                case "3": //ビックリ
+                    if (name == "妖精") {
+                        return "image/bikkuriyousei.png";
+                    } else {
+                        return "image/bikkurirobo.png";
+                    }
+                    break;
+                case "4": //ガッカリ
+                    if (name == "妖精") {
+                        return "image/gakkusiyousei.png";
+                    } else {
+                        return "image/gakkusirobo.png";
+                    }
+                    break;
+                case "5": //怒り
+                    if (name == "妖精") {
+                        return "image/ikariyousei.png";
+                    } else {
+                        return "image/ikarirobo.png";
+                    }
+                    break;
+            }
 
         }
 
-        clearTimeout(req);
-        i = 1;
-        message();
-      }
-    }
+        //セリフ切り取り関数
+        function message() {
 
-    function Timer() {
-      req = setTimeout("message_char()", speed);
-    }
+            //セリフ配列から一つずつ取り出す
+            string = serif_array[0].slice(0, 1).toString();
+            serif_array[0] = serif_array[0].slice(1);
 
-    //スキップしたいときの関数
-    function skip() {
-      localStorage.setItem("gameEnabled", "story");
-      location = "./../toStage.html";
-    }
-  </script>
+            //表情配列から一つずつ取り出す
+            faceNum = face_array[0].slice(0, 1).toString();
+            face_array[0] = face_array[0].slice(1);
+
+            message_char();
+
+        }
+
+        //セリフ表示関数
+        function message_char() {
+            const serif = document.getElementById("serifarea");
+
+            if (string.indexOf("妖精", 0) != -1) {
+                //妖精が喋っているとき
+                youseiImg.src = faceJudge(faceNum, "妖精");
+                //console.log();
+                //全体のcanvasを削除
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //再描画
+                ctx.drawImage(backImg, 0, 0, 850, 530);
+                ctx.drawImage(youseiImg, 400, 200, 450, 600);
+
+            } else if (string.indexOf("アンドロイド", 0) != -1) {
+                //アンドロイドが喋っているとき
+                robotImg.src = faceJudge(faceNum, "アンドロイド");
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(backImg, 0, 0, 850, 530);
+                ctx.drawImage(robotImg, 0, 200, 450, 600);
+            } else {
+                //それ以外
+                ctx.drawImage(backImg, 0, 0, 850, 530);
+            }
+
+            //セリフ表示
+            serif.innerHTML = string;
+
+            //セリフが最後まで終わっているか
+            if (i <= string.length) {
+                i++;
+                //Timer();
+            } else { //終わったとき
+                if (serif_array[0].length == 0) {
+                    serif.innerHTML = "スタートボタンを押すと、問題が始まります。";
+                    const next = document.getElementById("nextarea");
+                    const btn = document.createElement("button");
+                    btn.innerHTML = "スタート！";
+                    btn.onclick = function() {
+                        //ボタンが押された時
+                        location = "game.php";
+                    };
+                    next.appendChild(btn);
+
+                    //進むボタン無効化
+                    const startBtn = document.getElementById("startBtn");
+                    startBtn.disabled = true;
+                    return;
+
+                }
+
+                //clearTimeout(req);
+                i = 1;
+                message();
+            }
+        }
+
+        /*function Timer() {
+            req = setTimeout("message_char()", speed);
+        }*/
+
+        //スキップしたいときの関数
+        function skip() {
+            location = "game.php";
+        }
+    </script>
 </body>
 
 </html>
--- static/story/result.php
+++ static/story/result.php
@@ -1,259 +1,233 @@
 <?php
-session_start();
 
-require_once("./../../settings/env.php");
-
-$questionflg = $_SESSION["clear"];
-$userid = $_SESSION["userid"];
+if (isset($_POST["true"])) {
+    $questionflg = true;
+} else if (isset($_POST["false"])) {
+    $questionflg = false;
+}
 
 //正誤判定 正解ならtrue 不正解ならfalse
 //$questionflg = false;
 
-$questionNo = strval(intval($_SESSION["questionNo"]) + 1);
+//問題番号
+$questionNo =  $_POST["questionNo"];
 //データベースに接続
 $serif = [];
 //サーバーからセリフファイルを呼び出してstory.jsに送る
-$dsn = "mysql:dbname=LAA1355306-proggame;host=localhost";
-$user = "LAA1355306";
-$pass = $env["dbPassword"];
+$dsn = "mysql:dbname=programgame;localhost=3306";
+$user = "user";
+$pass = "user002";
 try {
-  $pdo = new PDO($dsn, $user, $pass);
-  if ($questionflg == true) {
-    //正解のとき
-    //sql文作成
-    $sql = "SELECT file FROM true_serif
+    $pdo = new PDO($dsn, $user, $pass);
+    if ($questionflg == true) {
+        //正解のとき
+        //sql文作成
+        $sql = "SELECT file,back_img FROM true_serif
             WHERE $questionNo = question_no";
-    //sql実行
-    $stmt = $pdo->query($sql);
+        //sql実行
+        $stmt = $pdo->query($sql);
 
-    foreach ($stmt as $row) {
-      $serif[] = explode(",", $row["file"]);
-    }
-
-    $sql = "UPDATE users SET story_progress = $questionNo where id = \"$userid\"";
-    $pdo->query($sql);
-    $questionNo++;
-    //print_r($serif);
-  } else {
-    //不正解のとき
-    //sql文作成
-    $sql = "SELECT file FROM false_serif
+        foreach ($stmt as $row) {
+            $serif[] = explode(",", $row["file"]);
+            $imgPath = $row["back_img"];
+        }
+        $questionNo++;
+    } else {
+        //不正解のとき
+        //sql文作成
+        $sql = "SELECT file,back_img FROM false_serif
             WHERE $questionNo = question_no";
-    //sql実行
-    $stmt = $pdo->query($sql);
+        //sql実行
+        $stmt = $pdo->query($sql);
 
-    foreach ($stmt as $row) {
-      $serif[] = explode(",", $row["file"]);
+        foreach ($stmt as $row) {
+            $serif[] = explode(",", $row["file"]);
+            $imgPath = $row["back_img"];
+        }
     }
-  }
 } catch (PDOException $e) {
-  echo $e->getMessage();
+    echo $e->getMessage();
 }
 
 $js_array = json_encode($serif);
 ?>
 
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>result</title>
-  <link rel="stylesheet" href="style.css">
-  <script type="text/javascript" src="./../js/LoginCheck.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>result</title>
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
-<style>
-  .modal {
-    transition: 0.5s;
-    opacity: 0;
-    position: absolute;
-    left: 0%;
-    top: 0%;
-    width: 100%;
-    height: 100%;
-    background-color: #f2feeeae;
-    z-index: -1;
-    text-align: center;
-  }
-
-  .center {
-    color: #561f16;
-    position: relative;
-    top: 40%;
-  }
-
-  .hintbody {
-    font-size: 1.2em;
-  }
-</style>
-
 <body onload="message()">
-  <div id="area">
-    <canvas id="can" width="850" height="530"></canvas>
-  </div>
-  <div id="serifarea"></div>
-  <div id="buttonarea"></div>
-  <script>
-    //キャンバスの作成
-    let canvas = document.getElementById("can");
-    let ctx = canvas.getContext("2d");
+    <div id="area">
+        <canvas id="can" width="850" height="530"></canvas>
+    </div>
+    <div id="serifarea"></div>
+    <div id="buttonarea"></div>
+    <script>
+        //キャンバスの作成
+        let canvas = document.getElementById("can");
+        let ctx = canvas.getContext("2d");
 
-    //画像の取得
-    let backImg = new Image();
-    let youseiImg = new Image();
-    let robotImg = new Image();
-    //パス設定
-    backImg.src = "image/mori.png";
-    youseiImg.src = "image/yousei.png";
-    robotImg.src = "image/robo.png";
+        //画像の取得
+        let backImg = new Image();
+        let youseiImg = new Image();
+        let robotImg = new Image();
+        let path = "<?php echo $imgPath; ?>";
 
-
-    //画像を読み込む前に描画しないようにloadイベントを行う
-    /*backImg.onload = function() {
-        //画像の描画
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-        ctx.drawImage(robotImg, 0, 200, 450, 600);
-        ctx.drawImage(youseiImg, 400, 200, 450, 600);
-
-    }*/
-
-    //phpから正誤判定のフラグ受け取り
-    let js_flg = "<?php var_export((bool)$questionflg); ?>";
-
-    const btnDiv = document.getElementById("buttonarea");
-    if (js_flg == "true") { //正解のとき
-      youseiImg.src = "image/nikkoriyousei.png";
-      ctx.drawImage(youseiImg, 400, 200, 450, 600);
-
-      //次へ進むボタン
-      /*const btn = document.createElement("button");
-      btn.innerHTML = "次へ進む";
-      btn.onclick = function() {
-          //
-          location = "geme.php";
-      };
-      btnDiv.appendChild(btn);*/
-
-    } else { //不正解のとき
-      //表情変える
-      youseiImg.src = "image/gakkusiyousei.png";
-      ctx.drawImage(youseiImg, 400, 200, 450, 600);
+        //パス設定
+        youseiImg.src = "image/yousei.png";
+        robotImg.src = "image/robo.png";
 
 
-      //ヒントボタンを作る
-      const hbtn = document.createElement("button");
-      hbtn.innerHTML = "ヒントを見る";
-      hbtn.onclick = function() {
-        document.getElementById("modal").style.opacity = "1";
-        document.getElementById("modal").style.zIndex = "1000";
-        
-        document.getElementById("hintText").innerText
-          = <?= '"' . $_SESSION["hinttext"] . '"' ?>;
-        
-        document.getElementById("modal").onclick = () => {
-          document.getElementById("modal").style.opacity = "0";
-          document.getElementById("modal").style.zIndex = "-1";
-        }
-      };
-      btnDiv.appendChild(hbtn);
+        //画像を読み込む前に描画しないようにloadイベントを行う
+        /*backImg.onload = function() {
+            //画像の描画
+            ctx.drawImage(backImg, 0, 0, 850, 530);
+            ctx.drawImage(robotImg, 0, 200, 450, 600);
+            ctx.drawImage(youseiImg, 400, 200, 450, 600);
 
-    }
+        }*/
 
-    //phpからセリフの配列受け取り
-    let array = <?php echo $js_array; ?>;
-    let speed = 100; //ミリ秒単位
-    let string, char;
-    let req;
-    let i = 0;
+        //phpから正誤判定のフラグ受け取り
+        let js_flg = "<?php var_export((bool)$questionflg); ?>";
 
-    function message() {
-      //セリフ配列から一つ目のセリフを取り出す
-      string = array[0].slice(0, 1).toString();
-      array[0] = array[0].slice(1);
+        const btnDiv = document.getElementById("buttonarea");
+        if (js_flg == "true") { //正解のとき
+            backImg.src = path;
+            youseiImg.src = "image/nikkoriyousei.png";
+            ctx.drawImage(youseiImg, 400, 200, 450, 600);
 
-      message_char();
-
-    }
-
-    function message_char() {
-      //console.log(i);
-      //char = string.slice(0, i);
-      //console.log(char);
-      let serif = document.getElementById("serifarea");
-
-      if (string.indexOf("妖精", 0) != -1) { //妖精が喋っているとき
-        //全体のcanvasを削除
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //再描画（背景）
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-
-        if (js_flg == "true") { //正解のとき 
-          //表情を変えて再描画
-          youseiImg.src = "image/nikkoriyousei.png";
-          ctx.drawImage(youseiImg, 400, 200, 450, 600);
+            //次へ進むボタン
+            /*const btn = document.createElement("button");
+            btn.innerHTML = "次へ進む";
+            btn.onclick = function() {
+                //
+                location = "geme.php";
+            };
+            btnDiv.appendChild(btn);*/
 
         } else { //不正解のとき
-          //表情を変えて再描画
-          youseiImg.src = "image/gakkusiyousei.png";
-          ctx.drawImage(youseiImg, 400, 200, 450, 600);
-        }
-
-      } else if (string.indexOf("アンドロイド", 0) != -1) {
-        //アンドロイドが喋っているとき
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-        ctx.drawImage(robotImg, 0, 200, 450, 600);
-      } else {
-        //それ以外
-        ctx.drawImage(backImg, 0, 0, 850, 530);
-      }
+            //表情変える
+            backImg.src = path;
+            youseiImg.src = "image/gakkusiyousei.png";
+            ctx.drawImage(youseiImg, 400, 200, 450, 600);
 
 
-      //セリフ表示
-      serif.innerHTML = string;
-      //}
-
-      if (i <= string.length) {
-        i++;
-        Timer();
-      } else {
-        if (array[0].length == 0) {
-          console.log("終わり");
-          return;
+            //ヒントボタンを作る
+            const hbtn = document.createElement("button");
+            hbtn.innerHTML = "ヒントを見る";
+            hbtn.onclick = function() {
+                //ヒントスペース
+            };
+            btnDiv.appendChild(hbtn);
 
         }
-        clearTimeout(req);
-        i = 1;
-        message();
-      }
-    }
 
-    function Timer() {
-      req = setTimeout("message_char()", speed);
-    }
-  </script>
-  <?php
-  if ($questionflg == true) {
-    //
-  ?>
-    <form action="./../toStage.html" method="post">
-      <input type="submit" value="次の問題へ">
-    </form>
-  <?php } else {
-    //
-  ?>
-    <form action="./../toStage.html" method="post">
-      <input type="submit" value="問題に戻る">
-    </form>
-  <?php } ?>
-  <div class="modal" id="modal">
-    <span class="center">ヒント：</span><br><br>
-    <span class="center hintbody" id="hintText"></span><br><br><br>
-    <span class="center">(画面をクリックすると閉じます)</span>
-  </div>
+        //phpからセリフの配列受け取り
+        let array = <?php echo $js_array; ?>;
+        let speed = 100; //ミリ秒単位
+        let string, char;
+        let req;
+        let i = 0;
+
+        function message() {
+            //セリフ配列から一つ目のセリフを取り出す
+            string = array[0].slice(0, 1).toString();
+            array[0] = array[0].slice(1);
+
+            message_char();
+
+        }
+
+        function message_char() {
+            //console.log(i);
+            //char = string.slice(0, i);
+            //console.log(char);
+            let serif = document.getElementById("serifarea");
+
+            if (string.indexOf("妖精", 0) != -1) { //妖精が喋っているとき
+                //全体のcanvasを削除
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //再描画（背景）
+                ctx.drawImage(backImg, 0, 0, 850, 530);
+
+                if (js_flg == "true") { //正解のとき 
+                    //表情を変えて再描画
+                    youseiImg.src = "image/nikkoriyousei.png";
+                    ctx.drawImage(youseiImg, 400, 200, 450, 600);
+
+                } else { //不正解のとき
+                    //表情を変えて再描画
+                    youseiImg.src = "image/gakkusiyousei.png";
+                    ctx.drawImage(youseiImg, 400, 200, 450, 600);
+                }
+
+            } else if (string.indexOf("アンドロイド", 0) != -1) { //アンドロイドが喋っているとき
+                //全体のcanvasを削除
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //再描画
+                ctx.drawImage(backImg, 0, 0, 850, 530);
+                
+                if (js_flg == "true") { //正解のとき 
+                    //表情を変えて再描画
+                    robotImg.src = "image/nikkorirobo.png";
+                    ctx.drawImage(robotImg, 0, 200, 450, 600);
+                }
+
+            } else {
+                //それ以外
+                ctx.drawImage(backImg, 0, 0, 850, 530);
+            }
+
+
+            //セリフ表示
+            serif.innerHTML = string;
+            //}
+
+            if (i <= string.length) {
+                i++;
+                Timer();
+            } else {
+                if (array[0].length == 0) {
+                    return;
+
+                }
+                clearTimeout(req);
+                i = 1;
+                message();
+            }
+        }
+
+        function Timer() {
+            req = setTimeout("message_char()", speed);
+        }
+    </script>
+    <?php
+    if ($questionflg == true) {
+
+    ?>
+        <form action="game.php" method="post">
+            <input type="submit" value="次の問題へ" class = "backBtn">
+            <input type="hidden" name="questionNo" value="<?php echo $questionNo; ?>">
+        </form>
+    <?php } else {
+
+    ?>
+        <form action="game.php" method="post" class="formBtn">
+            <input type="submit" value="問題に戻る" class = "backBtn">
+            <input type="hidden" name="questionNo" value="<?php echo $questionNo; ?>">
+        </form>
+    <?php } ?>
 </body>
 
 </html>
