852dfe693cc1ead3b37bf2a4d173fffc4fa3a065
WhiteF326 <Lume1a326.leno@gmail.com>
2021-10-28 16:16:39 +0900

    公式メッセージ機能追加

---
+++ static/js/OperatorMessage.js
@@ -0,0 +1,57 @@
+window.addEventListener("load", async () => {
+  // メッセージ番号未設定の場合
+  if (!localStorage.getItem("readMessage")) {
+    localStorage.setItem("readMessage", -1);
+  }
+
+  // メッセージ html を取得
+  const messageDOM = new DOMParser().parseFromString(
+    await (await fetch("./message.html")).text(), "text/html"
+  );
+
+  // メッセージ json を取得
+  const messages = JSON.parse(
+    await (await fetch("./../message/message.json")).text()
+  );
+
+  const lastMessageId = Math.max(...messages.map(r => r.id));
+
+  // 未読判定
+  if (Number(localStorage.getItem("readMessage")) < lastMessageId) {
+    // 未読処理
+    messageDOM.getElementById("readStatus").setAttribute(
+      "src", "img/message_unread.png"
+    );
+  }
+
+  // ページへの追加
+  document.body.appendChild(messageDOM.getElementsByTagName("table")[0]);
+
+  // クリック時の処理
+  const modalDOM = new DOMParser().parseFromString(
+    await (await fetch("./messageModal.html")).text(), "text/html"
+  );
+  const messageModal = modalDOM.getElementById("messageModal");
+  document.body.appendChild(messageModal);
+  
+  document.getElementById("readStatus").addEventListener("click", () => {
+    messageModal.style.zIndex = 810;
+    messageModal.style.opacity = 1;
+    // 既読にする
+    localStorage.setItem("readMessage", lastMessageId);
+    document.getElementById("readStatus").setAttribute(
+      "src", "img/message.png"
+    );
+  });
+  messageModal.addEventListener("click", () => {
+    messageModal.style.zIndex = -10000;
+    messageModal.style.opacity = 0;
+  });
+
+  // テキストの追加
+  document.getElementById("messageBody").appendChild(
+    document.createTextNode(
+      messages.find(r => r.id === lastMessageId)["body"]
+    )
+  );
+});
