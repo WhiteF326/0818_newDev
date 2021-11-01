dfe063c1cafafbbf0a4bbe8a795b3450ef9c20f8
WhiteF326 <Lume1a326.leno@gmail.com>
2021-11-01 11:05:24 +0900

    Revert "マップチップ変更に対応(インターフェースはクソ)"
    This reverts commit c65065493efed79cfe9a9ba35563d5d1542b1751.

--- static/js/ChipList.js
+++ static/js/ChipList.js
@@ -1,7 +1,19 @@
 class ChipList {
   // マップタイル用Imageオブジェクトの生成
-  constructor(chipData) {
-    this.chipData = chipData;
+  constructor() {
+    this.chipData = [
+      ["map01.png"],
+      ["map02.png"],
+      ["map03.png"],
+      ["spring.png"],
+      ["board0.png", "board1.png", "board2.png", "board3.png"],
+      ["switch1.png", "switch2.png", "switch3.png", "switch4.png", "switch5.png"],
+      ["door_off1.png", "door_off2.png", "door_off3.png", "door_off4.png", "door_off5.png"],
+      ["pushed1.png", "pushed2.png", "pushed3.png", "pushed4.png", "pushed5.png"],
+      ["door_on1.png", "door_on2.png", "door_on3.png", "door_on4.png", "door_on5.png"],
+      ["avoidCreate.png"],
+      ["map02.png"]
+    ];
     this.paramFunc = [
       (p) => 0,
       (p) => 0,
